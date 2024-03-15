78d1c497eeab427c741de894096cbd73f22191d1
Ollie Phillips <ollie@interject.io>
2024-03-15 11:04:43 +0000

    refine examples, and create the templates for automatic doc generation

--- examples/resources/morpheus_service_catalog_place_catalog_order/resource_multiple_items_ordered.tf
+++ examples/resources/morpheus_service_catalog_place_catalog_order/resource_multiple_items_ordered.tf
@@ -2,7 +2,7 @@ resource "morpheus_service_catalog_place_catalog_order" "tf_example_service_cata
 
   order_item {
     catalog_item_type_name = "Ubuntu 22.04"
-    config    = <<EOF
+    config                 = <<EOF
       {
         "customInstanceName": "My Ubuntu Instance",
         "optionTypeKey 2":" value 2",
@@ -13,7 +13,7 @@ resource "morpheus_service_catalog_place_catalog_order" "tf_example_service_cata
 
   order_item {
     catalog_item_type_id = "9"
-    config    = <<EOF
+    config               = <<EOF
       {
         "customInstanceName": "My Ubuntu Instance 2",
         "optionTypeKey 2":" value 2",
--- examples/resources/morpheus_service_catalog_place_catalog_order/resource_ordered_with_id.tf
+++ examples/resources/morpheus_service_catalog_place_catalog_order/resource_ordered_with_id.tf
@@ -2,7 +2,7 @@ resource "morpheus_service_catalog_place_catalog_order" "tf_example_service_cata
 
   order_item {
     catalog_item_type_id = "9"
-    config    = <<EOF
+    config               = <<EOF
       {
         "customInstanceName": "My Ubuntu Instance 2",
         "optionTypeKey 2":" value 2",
